9a5850b5ef10e16d90d9c4c0c448057c291f9b5f
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-01-23 10:02:02 +0100

    appearance change

---
+++ README.md
@@ -0,0 +1,49 @@
+# Toy dataset based on moving MNIST to evaluate models for Multi-Object Tracking and Segmentation
+
+**This is work in progress!!**
+
+This is an extension of the moving MNIST dataset by 
+
+[1] Srivastava, N., Mansimov, E., & Salakhudinov, R. (2015, June). Unsupervised learning of video 
+    representations using lstms. In International conference on machine learning (pp. 843-852).
+
+based on the implementation by [praateekmahajan](https://gist.github.com/praateekmahajan/b42ef0d295f528c986e2b3a0b31ec1fe) (thank you!).
+
+It is tailored towards the Multi-Object Tracking and Segmentation (MOTS) task introduced by 
+
+[2] Voigtlaender, P., Krause, M., Osep, A., Luiten, J., Sekar, B. B. G., Geiger, A., & Leibe, B. 
+    (2019). MOTS: Multi-object tracking and segmentation. In Proceedings of the IEEE Conference on 
+    Computer Vision and Pattern Recognition (pp. 7942-7951)
+
+This includes the following modifications:
+
+- (done) Numbers have a fixed order with regard to foreground to background
+- (todo) Numbers change their size while moving
+- (done) Numbers change their appearance while moving
+
+The generated data can be exported in COCO format or MOTS format.
+
+For COCO see:
+
+[3] Lin, T. Y., Maire, M., Belongie, S., Hays, J., Perona, P., Ramanan, D., ... & Zitnick, C. L. 
+    (2014, September). Microsoft coco: Common objects in context. In European conference on 
+    computer vision (pp. 740-755). Springer, Cham.
+
+# Installation
+
+```
+python3 -m venv path/to/venvs/moving-mnist-mots         # Create a virtual environment
+source path/to/venvs/moving-mnist-mots/bin/activate     # Activate the virtual environment
+pip install -r requirements.txt
+```
+
+# Configuration
+
+The configuration is controlled by `sacred`. For options see [config.yaml](config.yaml).
+
+# Generate data
+
+```
+python3 moving_mnist.py
+```
+
--- moving_mnist.py
+++ moving_mnist.py
@@ -88,30 +88,21 @@ def load_dataset(training=True):
         return load_mnist_images('train-images-idx3-ubyte.gz')
     return load_mnist_images('t10k-images-idx3-ubyte.gz')
 
-@ex.capture
-def generate_moving_mnist(training, shape=(64, 64),
-                          num_frames=30, 
-                          num_sequences=100, 
-                          original_size=28, 
-                          nums_per_image=2,
-                          brightness_band=(0, 1),
-                          seg_threshold=177.5):
-    '''
 
-    Args:
-        training: Boolean, used to decide if downloading/generating train set or test set
-        shape: Shape we want for our moving images (new_width and new_height)
-        num_frames: Number of frames in a particular movement/animation/gif
-        num_sequences: Number of movement/animations/gif to generate
-        original_size: Real size of the images (eg: MNIST is 28x28)
-        nums_per_image: Digits per movement/animation/gif.
+def generate_moving_mnist(config):
+    '''
 
     Returns:
         Dataset of np.uint8 type with dimensions num_frames * num_images x 1 x new_width x new_height
 
     '''
-    mnist = load_dataset(training)
-    width, height = shape
+    mnist = load_dataset(config["training"])
+    width, height = config["shape"]
+    original_size = config["original_size"]
+    num_frames = config["num_frames"]
+    num_sequences = config["num_sequences"]
+    digits_per_image = config["digits_per_image"]
+    brightness_band = config["brightness_band"]
 
     # Get how many pixels can we move around a single image
     lims = (x_lim, y_lim) = width - original_size, height - original_size
@@ -127,30 +118,31 @@ def generate_moving_mnist(training, shape=(64, 64),
 
     for img_idx in range(num_sequences):
         # Randomly generate direction, speed, velocity and brightness for all images
-        direcs = np.pi * (np.random.rand(nums_per_image) * 2 - 1)
-        speeds = np.random.randint(5, size=nums_per_image) + 2
+        direcs = np.pi * (np.random.rand(digits_per_image) * 2 - 1)
+        speeds = np.random.randint(5, size=digits_per_image) + 2
         veloc = np.asarray([(speed * math.cos(direc), speed * math.sin(direc)) for direc, speed in zip(direcs, speeds)])
-        brights = np.random.uniform(low=brightness_band[0], high=brightness_band[1], size=nums_per_image)
+        brights = np.random.uniform(low=brightness_band[0], high=brightness_band[1], size=digits_per_image)
+        brightness_increases = np.random.choice([True, False], size=digits_per_image)   # True for increase brightness
 
         # Get a list containing two PIL images randomly sampled from the database
         mnist_images = []
-        for i, r in enumerate(np.random.randint(0, mnist.shape[0], nums_per_image)):
+        for i, r in enumerate(np.random.randint(0, mnist.shape[0], digits_per_image)):
             img = Image.fromarray(get_image_from_array(mnist, r, mean=0))
             img = img.resize((original_size, original_size), Image.ANTIALIAS)
             mnist_images.append(img)
         
         # Generate tuples of (x,y) i.e initial positions for nums_per_image (default : 2)
-        positions = np.asarray([(np.random.rand() * x_lim, np.random.rand() * y_lim) for _ in range(nums_per_image)])
+        positions = np.asarray([(np.random.rand() * x_lim, np.random.rand() * y_lim) for _ in range(digits_per_image)])
 
         # Generate new frames for the entire num_frames
         for frame_idx in range(num_frames):
             image_id = img_idx * num_frames + frame_idx     # Id for coco annotations
 
             background = Image.new("L", (width, height))
-            masks = [Image.new("1", (width, height)) for _ in range(nums_per_image)]
+            masks = [Image.new("1", (width, height)) for _ in range(digits_per_image)]
 
             # In canv (i.e Image object) place the image at the respective positions
-            for i in range(nums_per_image):
+            for i in range(digits_per_image):
                 # Adjust image brightness first
                 img = ImageEnhance.Brightness(mnist_images[i]).enhance(brights[i])
                 
@@ -158,7 +150,7 @@ def generate_moving_mnist(training, shape=(64, 64),
                 background.paste(img, tuple(positions[i].astype(int)), mask=mnist_images[i])
 
                 # Save the ground truth mask
-                mask = mnist_images[i].point(lambda p: p > seg_threshold and 255)
+                mask = mnist_images[i].point(lambda p: p > config["seg_threshold"] and 255)
                 masks[i].paste(mask, tuple(positions[i].astype(int)))
                 
                 # Correct overlay by foreground mask
@@ -166,7 +158,7 @@ def generate_moving_mnist(training, shape=(64, 64),
                     masks[j] = ImageMath.eval("a & ~b", a = masks[j], b=masks[i])
             
             # Now do the annotations
-            for i in range(nums_per_image):
+            for i in range(digits_per_image):
                 id += 1
                 mask = cocotools.encode(np.asarray(masks[i]).astype(np.uint8).T)
                 mask["counts"] = mask["counts"].decode("utf-8")     # json is serializing it later
@@ -191,40 +183,34 @@ def generate_moving_mnist(training, shape=(64, 64),
             # Make the permanent change to position by adding updated velocity
             positions = positions + veloc
 
+            # Adjust brightness
+            for i, brightness in enumerate(brights):
+                # Switch brightness change to reduce if it hit the maximum
+                if brightness_increases[i] and brightness >= brightness_band[1]:
+                    brightness_increases[i] = False
+                # Same for minimum
+                elif not brightness_increases[i] and brightness <= brightness_band[0]:
+                    brightness_increases[i] = True
+                
+                # Now actually change the brightness
+                delta = config["brightness_change"] * brightness
+                brights[i] = brightness + delta if brightness_increases[i] else brightness - delta
+
             # Add the canvas to the dataset array
             dataset[image_id] = np.asarray(background).astype(np.uint8).T
 
     return dataset, annotations
 
 @ex.automain
-def main(training, dest, filetype='npz', frame_size=64, num_frames=30, num_sequences=100, original_size=28,
-         nums_per_image=2, seed=1):
-    np.random.seed(seed)
-    dat, annotations = generate_moving_mnist(training, num_frames=num_frames, num_sequences=num_sequences, \
-                                original_size=original_size, nums_per_image=nums_per_image)
-    n = num_sequences * num_frames
-    if filetype == 'npz':
+def main(config):
+    np.random.seed(config["seed"])
+    dat, annotations = generate_moving_mnist(config)
+    dest = config["dest"]
+    if config["filetype"] == 'npz':
         np.savez(dest, dat)
-    elif filetype == 'jpg':
+    elif config["filetype"] == 'jpg':
         for i in range(dat.shape[0]):
             Image.fromarray(get_image_from_array(dat, i, mean=0, norm=False)).save(os.path.join(dest, '{}.jpg'.format(i)))
 
     with open("annotations.json", "w") as f:
         json.dump(annotations, f)
-
-# if __name__ == '__main__':
-#     import argparse
-
-#     parser = argparse.ArgumentParser(description='Command line options')
-#     parser.add_argument('--dest', type=str, dest='dest', default='movingmnistdata')
-#     parser.add_argument('--filetype', type=str, dest='filetype', default="npz")
-#     parser.add_argument('--training', type=bool, dest='training', default=True)
-#     parser.add_argument('--frame_size', type=int, dest='frame_size', default=64)
-#     parser.add_argument('--num_frames', type=int, dest='num_frames', default=30)  # length of each sequence
-#     parser.add_argument('--num_images', type=int, dest='num_images', default=20000)  # number of sequences to generate
-#     parser.add_argument('--original_size', type=int, dest='original_size',
-#                         default=28)  # size of mnist digit within frame
-#     parser.add_argument('--nums_per_image', type=int, dest='nums_per_image',
-#                         default=2)  # number of digits in each frame
-#     args = parser.parse_args(sys.argv[1:])
-#     main(**{k: v for (k, v) in vars(args).items() if v is not None})
